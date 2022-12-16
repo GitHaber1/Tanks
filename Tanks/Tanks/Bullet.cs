28627a213fec59571b18b861bbd2c3eb0068bcb6
GitHaber1 <badindan2003@gmail.com>
2022-12-16 13:33:37 +0700

    Add Tank and Bullet classes

---
+++ Tanks/Tanks/Bullet.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tanks
+{
+    class Bullet : GameItem
+    {
+        public int reload;
+        public Tuple<int, int> coordinates { get; set; }
+        private Tuple<int, int> direction;
+        int speed { get; set; }
+        public Bullet()
+        {
+            reload = 0;
+            speed = 6;
+            width = 23;
+            height = 23;
+            direction = new Tuple<int, int>(0, 0);
+            coordinates = new Tuple<int, int>(-50, -50);
+        }
+        public void Reloading()
+        {
+            reload++;
+        }
+        public bool isVisible(Tuple<int, int> coord)
+        {
+            if (coord.Item1 > 1140 || coord.Item2 > 740 || coord.Item1 < 1 || coord.Item2 < 0)
+            {
+                return false;
+            }
+            else
+                return true;
+        }
+        public void Destroy()
+        {
+            Reloading();
+            coordinates = new Tuple<int, int>(-50, -50);
+            direction = new Tuple<int, int>(0, 0);
+        }
+        public void Movement()
+        {
+            int X = coordinates.Item1;
+            int Y = coordinates.Item2;
+            if (coordinates.Item1 > -10)
+                coordinates = new Tuple<int, int>(X + direction.Item1 * speed, Y + direction.Item2 * speed);
+        }
+        public void Spawn(Tuple<int, int> dir, Tuple<int, int> p1)
+        {
+            direction = dir;
+            coordinates = new Tuple<int, int>(p1.Item1, p1.Item2);
+        }
+        public override void DrawItem(Graphics item, PictureBox pic)
+        {
+            Bitmap img = new Bitmap("Bullet.png");
+            item.DrawImage(img, coordinates.Item1, coordinates.Item2);
+        }
+    }
+}
---
+++ Tanks/Tanks/Tank.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tanks
+{
+    class Tank : GameItem
+    {
+        public Rectangle NextRadius { get; set; }
+        public Rectangle Radius { get; set; }
+        public bool isDead { get; set; }
+        public Tuple<int, int> coordinates { get; set; }
+        public Bullet bullet { get; set; }
+        public int Mode { get; set; }
+        public int Speed { get; set; }
+        public Tank()
+        {
+            isDead = false;
+            bullet = new Bullet();
+            Speed = 2;
+            Mode = 3;
+            width = 60;
+            height = 60;
+        }
+        public Tuple<int, int> GetFirePosition(PictureBox pic)
+        {
+            Tuple<int, int> dir;
+            if (Mode == 1)
+                dir = new Tuple<int, int>(pic.Location.X + pic.Width / 2 + 40, pic.Location.Y + pic.Width / 2 - 10);
+            else if (Mode == 2)
+                dir = new Tuple<int, int>(pic.Location.X - pic.Width / 2, pic.Location.Y + pic.Width / 2 - 10);
+            else if (Mode == 3)
+                dir = new Tuple<int, int>(pic.Location.X + pic.Width / 2 - 10, pic.Location.Y - pic.Width / 2);
+            else
+                dir = new Tuple<int, int>(pic.Location.X + pic.Width / 2 - 10, pic.Location.Y + pic.Width / 2 + 40);
+            return dir;
+        }
+        public Tuple<int, int> GetDirection()
+        {
+            Tuple<int, int> dir;
+            if (Mode == 1)
+                dir = new Tuple<int, int>(1, 0);
+            else if (Mode == 2)
+                dir = new Tuple<int, int>(-1, 0);
+            else if (Mode == 3)
+                dir = new Tuple<int, int>(0, -1);
+            else
+                dir = new Tuple<int, int>(0, 1);
+            return dir;
+        }
+        public bool isValidMove(Tuple<int, int> coord)
+        {
+            if (coord.Item1 > 1120 || coord.Item2 > 700 || coord.Item1 < 1 || coord.Item2 < 1)
+            {
+                return false;
+            }
+            else
+                return true;
+        }
+        public override void DrawItem(Graphics item, PictureBox pic)
+        {
+        }
+    }
+}

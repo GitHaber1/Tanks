93a83e2c6ce6038946530293beac760f804cc907
GitHaber1 <badindan2003@gmail.com>
2022-12-16 13:11:38 +0700

    Add Bricks class

---
+++ Tanks/Tanks/Bricks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tanks
+{
+    class Bricks : GameItem
+    {
+        public Bricks()
+        {
+            width = 65;
+            height = 64;
+        }
+        public override void DrawItem(Graphics item, PictureBox pic)
+        {
+            Bitmap img = new Bitmap("brick.png");
+            pic.Size = new Size(width, height);
+            pic.Image = img;
+        }
+    }
+}

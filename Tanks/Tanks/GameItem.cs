028efab8feee556e61b6f37941415f3ad82191cf
accaount <danya.shitts@gmail.com>
2022-12-16 12:31:30 +0700

    add class GameItem

---
+++ Tanks/Tanks/GameItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanks
+{
+    abstract class GameItem
+    {
+        public abstract void DrawItem(Graphics item, PictureBox pic);
+        public int width { get; set; }
+        public int height { get; set; }
+    }
+}

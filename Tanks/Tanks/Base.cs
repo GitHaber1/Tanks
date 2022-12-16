b23569dcf34b7a344e060c3d0d88c07acb6e5d35
accaount <danya.shitts@gmail.com>
2022-12-16 13:28:53 +0700

    Add Wolls and Base classes

---
+++ Tanks/Tanks/Base.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tanks
+{
+    class Base : GameItem
+    {
+        public bool isDestroyed { get; set; }
+        public Base()
+        {
+            isDestroyed = false;
+            width = 64;
+            height = 59;
+        }
+        public override void DrawItem(Graphics item, PictureBox pic)
+        {
+            Bitmap img = new Bitmap("base.png");
+            pic.Size = new Size(width, height);
+            pic.Image = img;
+        }
+    }
+}
---
+++ Tanks/Tanks/BlockWoll.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tanks
+{
+    class BlockWall : GameItem
+    {
+        public BlockWall()
+        {
+            width = 64;
+            height = 64;
+        }
+        public override void DrawItem(Graphics item, PictureBox pic)
+        {
+            Bitmap img = new Bitmap("wall.png");
+            pic.Size = new Size(width, height);
+            pic.Image = img;
+        }
+    }
+}
